0ca3a624a653388edc1e0eeed9b36ab310063ecf
mkucharz <m@kucharz.net>
2017-06-15 19:36:47 +0200

    Initial version.

---
+++ scripts/send.js
@@ -0,0 +1,43 @@
+/* global CONFIG, META */
+import fetch from 'node-fetch'
+import FormData from 'form-data'
+import {response} from 'syncano-server'
+
+
+const {MAILGUN_API_KEY, MAILGUN_DOMAIN} = CONFIG
+
+const url = `https://api:${MAILGUN_API_KEY}@api.mailgun.net/v3/${MAILGUN_DOMAIN}/messages`
+const data = new FormData()
+const params = ['from', 'to', 'subject', 'html']
+
+for (const key of params) {
+  const value = ARGS[key]
+
+  if (value) {
+    data.append(key, value)
+  }
+}
+
+fetch(url, {method: 'POST', body: data})
+  .then(checkStatus)
+  .then(parseJSON)
+  .then(res => {
+    response.json({message: "Message was sent."})
+  })
+  .catch(err => {
+    response.json({message: err.message})
+  })
+
+function parseJSON(response) {
+  return response.json()
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
